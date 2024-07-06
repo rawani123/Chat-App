d9201e0269f3c674eefdef47b9ed8c18b2424f79
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-07-06 18:11:55 +0530

    done

--- server/index.js
+++ server/index.js
@@ -13,12 +13,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: "*",
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
